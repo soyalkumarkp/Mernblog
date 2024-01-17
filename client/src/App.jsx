ea27f62d3ffc34f56114a434b03a8f665b91a570
soyakkumarkp <soyalkumarkp@gmail.com>
2024-01-17 06:06:48 +0530

    installed react and tailwisnd css

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl">
+      soyal <span className="text-red-500">loves</span> shruthi
+    </h1>
+  );
+};
+
+export default App;
